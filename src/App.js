4fd71a3955f971ae04b87d24ec899433eb9a3ad3
Milinda Ranawaka <it18018288@my.sliit.lk>
2021-10-04 15:01:55 +0530

    Rounded Button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './App.css';
-import ToggleSwitch from './components/Toggle/toggle';
-import Button from './components/Button/Button';
-import ButtonCreative from './components/StyledButton/Button';
+import React from "react";
+import "./App.css";
+import ToggleSwitch from "./components/Toggle/toggle";
+import Button from "./components/Button/Button";
+import ButtonCreative from "./components/StyledButton/Button";
 
 function App() {
-	return (
-		<div>
-			<h1>Components showcase</h1>
-			<Button title='Click me' />
-			<ToggleSwitch />
-			<ButtonCreative children="New Button"/>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Components showcase</h1>
+      <Button title="Click me" />
+      <ToggleSwitch />
+      <ButtonCreative children="New Button" />
+      <ButtonCreative btnColor="blue" type="outline">
+        Outline Button
+      </ButtonCreative>
+    </div>
+  );
 }
 
 export default App;
