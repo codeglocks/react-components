cf691ffef584d17cd92c6d3ede253b38d2b7621f
Milinda Ranawaka <it18018288@my.sliit.lk>
2021-10-05 17:57:28 +0530

    Toggle Switch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import ToggleSwitch from "./components/Toggle/toggle";
 import Button from "./components/Button/Button";
 import ButtonCreative from "./components/StyledButton/Button";
+import ToggleSwitch2 from "./components/ToggleSwitch/ToggleSwitch"
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <ButtonCreative btnColor="blue" type="outline">
         Outline Button
       </ButtonCreative>
+
+      <ToggleSwitch2 id="a02" name="Toggle" onChange={e => console.log("onToggle")}/>
     </div>
   );
 }
